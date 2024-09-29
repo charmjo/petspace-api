ab01ce197a0e94d6df3ce220171c2139b4be3b78
charmjo <charmjohannes.relator@gmail.com>
2024-09-28 23:54:36 -0400

    added fortifyservice provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,6 @@
 <?php
-
+//TODO Charm : note to self, DO NOT FORGET TO ADD THE SERVICE PROVIDER for every package installed in this project
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\FortifyServiceProvider::class
 ];
