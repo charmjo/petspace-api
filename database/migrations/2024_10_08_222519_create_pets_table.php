e1bfd2b279f9d54e220180b4220ac76c85abfa9d
charmjo <charmjohannes.relator@gmail.com>
2024-10-08 22:33:57 -0400

    setup Pet model

---
+++ database/migrations/2024_10_08_222519_create_pets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pet_owner_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('name');
+            $table->string('breed');
+            $table->string('animal_type');
+            $table->timestamp('dob')->nullable();
+            $table->string('color')->nullable();
+            $table->string('gender')->nullable(); // I'd rather have the front end deal with this
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pets');
+    }
+};
