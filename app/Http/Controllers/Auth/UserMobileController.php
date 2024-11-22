9bb9b02ed667e04fed34207228da45bac3d2a238
charmjo <charmjohannes.relator@gmail.com>
2024-11-22 17:05:07 -0500

    updated bio with nullable

--- app/Http/Controllers/Auth/UserMobileController.php
+++ app/Http/Controllers/Auth/UserMobileController.php
@@ -19,21 +19,21 @@ use App\Models\User;
 class UserMobileController extends Controller
 {
     // why am I forced to create zis?
-    /* 
+    /*
         - Laravel fortify's registration contains a stateful guard which means, a cookie is needed to access this class.
         - Mobile tokens are cookie-less there will be an error accessing this route. so, I will have to MAKE MY. OWN.
         - unya nako maghuna-huna og protect once I got this thing to work!
     */
     public function createUser(Request $request,
-        CreatesNewUsers $creator) 
+        CreatesNewUsers $creator)
     {
         // so, I need to return the validations here coz it is sure as hell not returning it.
         // anyways, createnewuser is just an impelementation of createsnewusers and is not beholden to anything so this should be fine???
         event(new Registered($user = $creator->create($request->all())));
-        
+
         // will have to return a json token. I sincerely pray to god this works.
         return response()->json(
-        ['message' => 'user is now registered'
+        ['message' => 'You are now registered.'
         ]);
     }
 
@@ -44,22 +44,22 @@ class UserMobileController extends Controller
             'password' => 'required',
             'device_name' => 'required',
         ]);
-    
+
         $user = User::where('email', $request->email)->first();
-    
+
         if (! $user || ! Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
                 'email' => ['The provided credentials are incorrect.'],
             ]);
         }
-    
+
         // restrict user to only one token
         if($user->tokens()->count() >= 1){
             $user->tokens()->delete();
         };
-    
+
         $userToken = $user->createToken($request->device_name)->plainTextToken;
-    
+
         return response()->json([
             'token' => $userToken
         ])->header('Content-Type', 'application/json');
